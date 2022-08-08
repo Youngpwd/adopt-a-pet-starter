2489811e436439172c8623a3af98afcc5e52e133
Charles Young <cbyoung415@gmail.com>
2022-08-08 09:20:12 -0700

    using Location object to get query params

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -3,22 +3,19 @@ import Hero from '../../components/hero';
 import { getPets } from '../../api/petfinder';
 import Pet from '../../components/pet';
 
-// import useLocation here
+import { useLocation } from 'react-router-dom';
 
 const SearchPage = () => {
-
-  // Get the search value from useLocation() here
-  const search = 'REPLACE ME';
-
-  const queryParams = useMemo(() => { 
-    return new URLSearchParams('REPLACE ME');
+  const { search } = useLocation();
+  const queryParams = useMemo(() => {
+    return new URLSearchParams(search);
   }, [search]);
 
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
     async function getPetsData() {
-      const petNameToFind = 'REPLACE ME';
+      const petNameToFind = queryParams.get('name');
       const petsData = await getPets('', petNameToFind);
 
       setPets(petsData);
