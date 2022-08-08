66c9c4dc595e7ad8312817933e579528f88e0408
Charles Young <cbyoung415@gmail.com>
2022-08-08 09:19:15 -0700

    add SearchPage route

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
           <Route path="/:type/:id">
             <PetDetailsPage />
           </Route>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
           <Route path="/:type?">
             <HomePage />
           </Route>
